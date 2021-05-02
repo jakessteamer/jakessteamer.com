35cce31fe252bb02337c725460551e7cdcef83b8
andrewriveradev <softwarengdev@gmail.com>
2021-05-02 14:06:41 -0600

    Add sitemap, remove csp

--- next.config.js
+++ next.config.js
@@ -1,57 +1,16 @@
 /* eslint-disable global-require */
 /* eslint-disable no-use-before-define */
-const { createSecureHeaders } = require('next-secure-headers');
 
 module.exports = {
     target: 'serverless',
+    strictPostcssConfiguration: true,
     future: {
         webpack5: true
     },
-    poweredByHeader: false
-};
-
-// https://securityheaders.com
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://housecallpro.com  https://*.housecallpro.com https://plausible.io https://*.netlify.app;
-  child-src  https://*.housecallpro.com https://plausible.io;
-  style-src 'self' 'unsafe-inline' https://*.housecallpro.com https://plausible.io https://*.netlify.app;
-  img-src * blob: data:;
-  media-src 'none';
-  connect-src * https://plausible.io;
-  font-src 'self';
-`;
-
-const securityHeaders = [
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-    {
-        key: 'Referrer-Policy',
-        value: 'origin-when-cross-origin'
-    },
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-    {
-        key: 'X-Frame-Options',
-        value: 'SAMEORIGIN'
-    },
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-    {
-        key: 'X-Content-Type-Options',
-        value: 'nosniff'
-    },
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-    {
-        key: 'X-DNS-Prefetch-Control',
-        value: 'on'
-    },
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-    {
-        key: 'Strict-Transport-Security',
-        value: 'max-age=31536000; includeSubDomains; preload'
-    },
-    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-    // Opt-out of Google FLoC: https://amifloced.org/
-    {
-        key: 'Permissions-Policy',
-        value: 'camera=(), microphone=(), geolocation=(), interest-cohort=()'
+    poweredByHeader: false,
+    webpack: (config, { dev, isServer }) => {
+        if (isServer) {
+            require('./scripts/generate-sitemap');
+        }
     }
-];
+};
--- pages/_document.js
+++ pages/_document.js
@@ -3,70 +3,26 @@
 /* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import crypto from 'crypto';
-import { v4 } from 'uuid';
-/**
- * Generate Content Security Policy for the app.
- * Uses randomly generated nonce (base64)
- *
- * @returns [csp: string, nonce: string] - CSP string in first array element, nonce in the second array element.
- */
-const generateCsp = () => {
-    const production = process.env.NODE_ENV === 'production';
 
-    // generate random nonce converted to base64. Must be different on every HTTP page load
-    const hash = crypto.createHash('sha256');
-    hash.update(v4());
-    const nonce = hash.digest('base64');
-
-    const DevelopmentContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://housecallpro.com  https://*.housecallpro.com https://plausible.io;
-  child-src  https://*.housecallpro.com https://plausible.io;
-  style-src 'self' 'unsafe-inline' https://*.housecallpro.com https://plausible.io;
-  img-src * blob: data:;
-  media-src 'none';
-  connect-src * https://plausible.io;
-  font-src 'self';
-`;
-
-    let csp = ``;
-    csp += `default-src 'self';`;
-    csp += `base-uri 'none';`;
-    csp += `style-src https://housecallpro.com  https://*.housecallpro.com 'unsafe-inline';`; // NextJS requires 'unsafe-inline'
-    csp += `script-src 'nonce-${nonce}' 'self' https://housecallpro.com  https://*.housecallpro.com https://plausible.io https://plausible.io/js/plausible.outbound-links.js ${
-        production ? '' : "'unsafe-eval'"
-    };`; // NextJS requires 'self' and 'unsafe-eval' in dev (faster source maps)
-    csp += `child-src https://*.housecallpro.com https://*.housecallpro.com  https://plausible.io https://plausible.io/js/plausible.outbound-links.js;`;
-    csp += `font-src 'self';`;
-    csp += `img-src * blob: data:;`;
-    csp += `connect-src 'self' https://housecallpro.com  https://*.housecallpro.com https://plausible.io https://plausible.io/js/plausible.outbound-links.js;`;
-
-    return [csp, nonce];
-};
 class MyDocument extends Document {
     render() {
-        const [csp, nonce] = generateCsp();
         return (
             <Html lang="en">
-                <Head>
-                    <meta httpEquiv="Content-Security-Policy" content={csp} />
+                <Head />
+                <body className="leading-normal tracking-normal text-white">
+                    <Main />
+                    <NextScript />
                     <script
                         async
                         defer
                         data-domain="jakessteamer.com"
-                        nonce={nonce}
                         src="https://plausible.io/js/plausible.outbound-links.js"
                     />
 
                     <script
-                        nonce={nonce}
                         src="https://www.housecallpro.com/assets/BookingBar.js?company=e5fb241079164c83aa85e58e9aa1b12b&path=Jakes-Steamer"
+                        defer
                     />
-                </Head>
-                <body className="leading-normal tracking-normal text-white">
-                    <Main />
-                    <NextScript nonce={nonce} />
                 </body>
             </Html>
         );
