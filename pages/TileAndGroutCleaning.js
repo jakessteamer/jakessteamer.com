b97bcf7d2504bb718bdb6e4db0c01b704eb01eeb
andrewriveradev <softwarengdev@gmail.com>
2021-07-27 06:46:30 -0600

    update upholstery page copy

--- pages/TileAndGroutCleaning.js
+++ pages/TileAndGroutCleaning.js
@@ -23,48 +23,64 @@ export default function TileAndGroutCleaning() {
           grease and food on the floor and this gets pressed into your grout. In
           places like entryways, mud and dirt tend to discolor grout.
         </p>
-        <h2 className="content-title">Tile and Grout Cleaning Process</h2>
-        <p>
-          We first apply a non-toxic cleaner to your tile and grout to break up
-          any dirt or grease stuck in the grout. We then deep clean your tile
-          and grout with high-speed machine that scrubs and extracts the dirt
-          from your tile and grout. For particularly stubborn areas, vapor steam
-          cleaning may be used. Once the surface is completely clean we will
-          apply our grout sealant to protect your floors from future spills and
-          grime.
-        </p>
+
+        <div className="flex flex-col ">
+          <p>
+            What makes tile cleaning and grout cleaning so difficult? Dirt and
+            contaminants build up over time, making your tiles look dull, and
+            causing grout to darken and appear discolored. Home cleaning methods
+            simply can't get at the built-up, ground-in dirt that has settled in
+            hard-to-reach places. Dirt trapped in the pores of tile and grout is
+            especially difficult to remove with traditional cleaning solutions.
+          </p>
+          <div className="aspect-w-1 aspect-h-1 my-12">
+            <img src="/images/tile/kitchen-tile-cleaning-1.jpg" alt="" />
+          </div>
+          <h2 className="content-title">Tile and Grout Cleaning Process</h2>
+          <p>
+            We first apply a non-toxic cleaner to your tile and grout to break
+            up any dirt or grease stuck in the grout. We then deep clean your
+            tile and grout with high-speed machine that scrubs and extracts the
+            dirt from your tile and grout.
+          </p>
+          <div className="aspect-w-1 aspect-h-1 my-12">
+            <img src="/images/tile/kitchen-tile-cleaning-2.jpg" alt="" />
+          </div>
+          <p>
+            We complete the job with a high-pressure rinse.After cleaning your
+            tile and grout, our professional technicians will apply a
+            penetrating sealer to protect grout against mildew and staining for
+            up to two years. And basic Grout Cleaning isn't the only service we
+            provide. If you have colored grout in your kitchen or bathroom or
+            between other tile surfaces, Jake’s steamer Tile Cleaning service
+            will safely get ceramic and porcelain tiled surfaces sparkling
+            again! And if your tile is made of natural material such as granite,
+            marble, slate, travertine or Saltillo, no problem!
+          </p>
+          <div className="aspect-w-1 aspect-h-1 my-12">
+            <img src="/images/tile/shower-tile-cleaning-1.png" alt="" />
+          </div>
+          <div className="aspect-w-1 aspect-h-1 my-12">
+            <img src="/images/tile/shower-tile-cleaning-2.jpeg" alt="" />
+          </div>
+        </div>
+
         {/* <p>
           Professional Tile Cleaning and Grout Cleaning process will keep your
           tile surfaces, grout lines and countertops clean and fresh. We can
           boldly go where bottled cleaners and household mops have never gone
           before!
+          Once the surface is completely
+            clean we will apply our grout sealant to protect your floors from
+            future spills and grime.
         </p> */}
 
-        <p>
-          What makes tile cleaning and grout cleaning so difficult? Dirt and
-          contaminants build up over time, making your tiles look dull, and
-          causing grout to darken and appear discolored. Home cleaning methods
-          simply can't get at the built-up, ground-in dirt that has settled in
-          hard-to-reach places. Dirt trapped in the pores of tile and grout is
-          especially difficult to remove with traditional cleaning solutions.
-        </p>
         <p>
           Thankfully, Jake’s Steamer powerful truck-mounted cleaning system
           safely cleans floors, showers, and countertops by quickly removing
           dirt and contaminants. Our Tile Cleaning and Grout Cleaning process
           uses heat, a specialized cleaning solution, and vacuum power to remove
           the dirt that homeowners can't.
-        </p>
-        <p>
-          We complete the job with a high-pressure rinse.After cleaning your
-          tile and grout, our professional technicians will apply a penetrating
-          sealer to protect grout against mildew and staining for up to two
-          years. And basic Grout Cleaning isn't the only service we provide. If
-          you have colored grout in your kitchen or bathroom or between other
-          tile surfaces, Jake’s steamer Tile Cleaning service will safely get
-          ceramic and porcelain tiled surfaces sparkling again! And if your tile
-          is made of natural material such as granite, marble, slate, travertine
-          or Saltillo, no problem!
         </p>
 
         {/* <footer>
