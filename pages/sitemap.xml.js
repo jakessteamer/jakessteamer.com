cafc9b26848f044d463cb62ef7297286b0a44b88
andrewriveradev <softwarengdev@gmail.com>
2021-08-03 20:35:17 -0600

    update sitemap settings

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,37 +1,45 @@
 import React from "react";
-import fs from "fs";
+// import fs from "fs";
+import globby from "globby";
 
 const Sitemap = () => {};
 
-export const getServerSideProps = ({ res }) => {
+export const getServerSideProps = async ({ res }) => {
   const baseUrl = {
     development: "http://localhost:3000",
     production: "https://jakessteamer.com",
   }[process.env.NODE_ENV];
+  // const getPages = async () => {
+  //   const pages = await globby(["pages/*.js", "!pages/_*.js", "!pages/api"]);
+  //   return pages;
+  // };
+  const pages = await globby(["pages/*.js", "!pages/_*.js", "!pages/api"]);
 
-  const staticPages = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return !["_app.js", "_document.js", "_error.js"].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      return `${baseUrl}/${staticPagePath}`;
-    });
+  // const staticPages = fs
+  //   .readdirSync(pages)
+  //   .filter((staticPage) => {
+  //     return !["_app.js", "_document.js", "_error.js"].includes(staticPage);
+  //   })
+  //   .map((staticPagePath) => {
+  //     return `${baseUrl}/${staticPagePath}`;
+  //   });
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${staticPages
-        .map((url) => {
-          return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `;
-        })
-        .join("")}
+    ${pages
+      .map((page) => {
+        const path = page.replace("pages", "").replace(".js", "");
+        const routePath = path === "index" ? "" : path;
+        return `
+          <url>
+            <loc>${baseUrl}${routePath}</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <changefreq>monthly</changefreq>
+            <priority>1.0</priority>
+          </url>
+        `;
+      })
+      .join("")}
     </urlset>
   `;
 
