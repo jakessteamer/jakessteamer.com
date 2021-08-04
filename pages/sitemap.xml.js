25bf76d3bc0b61516c6ae12bd6324b5c911e1b5b
andrewriveradev <softwarengdev@gmail.com>
2021-08-03 21:01:47 -0600

    update sitemap settings

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,51 +1,60 @@
 import React from "react";
-// import fs from "fs";
-import globby from "globby";
+import fs from "fs";
 
 const Sitemap = () => {};
 
-export const getServerSideProps = async ({ res }) => {
+export const getServerSideProps = ({ res }) => {
   const baseUrl = {
-    development: "http://localhost:3000",
-    production: "https://jakessteamer.com",
+    development: "http://localhost:5000",
+    production: "https://mydomain.com",
   }[process.env.NODE_ENV];
-  // const getPages = async () => {
-  //   const pages = await globby(["pages/*.js", "!pages/_*.js", "!pages/api"]);
-  //   return pages;
-  // };
-  if (process.env.NODE_ENV === "development") {
-    const pages = await globby(["pages/*.js", "!pages/_*.js", "!pages/api"]);
-  }
-  const pages = await globby(["./*.js", "!./_*.js", "!./api"]);
+
   // const staticPages = fs
-  //   .readdirSync(pages)
-  //   .filter((staticPage) => {
-  //     return !["_app.js", "_document.js", "_error.js"].includes(staticPage);
-  //   })
-  //   .map((staticPagePath) => {
-  //     return `${baseUrl}/${staticPagePath}`;
-  //   });
+  // .readdirSync({
+  //   development: 'pages',
+  //   production: './',
+  // }[process.env.NODE_ENV])
+  // .filter((staticPage) => {
+  //   return ![
+  //     "_app.js",
+  //     "_document.js",
+  //     "_error.js",
+  //     "sitemap.xml.js",
+  //   ].includes(staticPage);
+  // })
+  // .map((staticPagePath) => {
+  //   return `${baseUrl}/${staticPagePath}`;
+  // });
+  const staticPages = fs
+    .readdirSync("pages")
+    .filter((staticPage) => {
+      return ![
+        "_app.js",
+        "_document.js",
+        "_error.js",
+        "sitemap.xml.js",
+      ].includes(staticPage);
+    })
+    .map((staticPagePath) => {
+      return `${baseUrl}/${staticPagePath}`;
+    });
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    ${pages
-      .map((page) => {
-        const path = page.replace("pages", "").replace(".js", "");
-        const routePath = path === "index" ? "" : path;
-        return `
-          <url>
-            <loc>${baseUrl}${routePath}</loc>
-            <lastmod>${new Date().toISOString()}</lastmod>
-            <changefreq>monthly</changefreq>
-            <priority>1.0</priority>
-          </url>
-        `;
-      })
-      .join("")}
+      ${staticPages
+        .map((url) => {
+          return `
+            <url>
+              <loc>${url}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
     </urlset>
   `;
-
-  // console.log(sitemap)
 
   res.setHeader("Content-Type", "text/xml");
   res.write(sitemap);
