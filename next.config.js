7ec62034d5e539325c813b9dbbac81623820bfc3
andrewriveradev <softwarengdev@gmail.com>
2021-08-03 21:12:07 -0600

    update sitemap settings

--- next.config.js
+++ next.config.js
@@ -9,14 +9,4 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { isServer }) => {
-    // Fixes npm packages that depend on `fs` module
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
-  },
 };
